85018b192c244fc012a3bbf81ba668373a4dbb2c
Your Name <zcabdla@ucl.ac.uk>
2020-01-19 10:11:36 +0000

    Own user posts and deleting posts

--- marketplace/views.py
+++ marketplace/views.py
@@ -10,20 +10,25 @@ class ListingListView(APIView):
     parser_classes = (MultiPartParser, FormParser)
 
     def get(self, request):
-        listings = Listing.objects.all()
+        search_terms = request.GET.get('search', None)
+        user = request.GET.get('user', None)
+
+        if user == None:
+            listings = Listing.objects.all()
+        else:
+            listings = Listing.objects.filter(author_email=user)
+
         serializer = ListingSerializer(listings, many=True)
         for listing_entry in serializer.data:
             listing_entry["listing_image"] = request.build_absolute_uri('/')[:-1].strip("/") + \
                                              listing_entry["listing_image"]
 
-        print(serializer.data)
-
-        search_terms = request.GET.get('search', None)
         # if search_terms is not None:
         #     serializer.data = [listing for listing in serializer.data if search_terms in listing["listing_title"]]
 
         return Response([listing for listing in serializer.data if search_terms in listing["listing_title"]] if
                         search_terms is not None else serializer.data)
+
 
     def post(self, request, *args, **kwargs):
         file_serializer = ListingSerializer(data=request.data)
@@ -34,3 +39,8 @@ class ListingListView(APIView):
             return Response(file_serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+        listing = Listing.objects.get(listing_id=request.DELETE.get('listingId', None))
+        listing.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
